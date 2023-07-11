64c57f9bb68515e9d7339b75d5abfcb6ce545b24
Hemalatha <mhemalatha520@gmail.com>
2023-07-11 16:40:06 +0530

    First Commit

---
+++ HemaProject/src/Calculator.java
@@ -0,0 +1,15 @@
+public class Calculator {
+	int n1=3;
+	int n2=4;
+	public void add()
+	{
+		int sum=n1+n2;
+	}
+
+	public static void main(String[] args) {
+	
+     Calculator s=new Calculator();
+     s.add();
+	}
+
+}
